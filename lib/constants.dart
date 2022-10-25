c7a4c646e94ce437beae1d436da3c8b1c8a9eb9e
櫻井 雛羽 <2200496@ecc.ac.jp>
2022-10-25 11:41:43 +0900

    💄constans

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,3 +3,5 @@ import 'package:flutter/material.dart';
 //カラー変数
 const kAppbarColor = Color(0xFFFBD579);
 const kTitleColor = Color(0xFFFFFF88);
+const kBackgroundColor = Color(0xFFFFFAE3);
+const kFontColor = Color(0xFF666666);

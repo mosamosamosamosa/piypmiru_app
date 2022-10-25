56e7b87c52e765c3e5ff2d583700066b8bf1aa7e
櫻井 雛羽 <2200496@ecc.ac.jp>
2022-10-25 11:40:15 +0900

    ✨ノーマルなボタン

---
+++ lib/components/nomal_button.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:piyomiru_application/constants.dart';
+
+class NomalButton extends StatelessWidget {
+  NomalButton({
+    Key? key,
+    required this.text,
+    required this.pushable,
+  }) : super(key: key);
+
+  final String text;
+  final bool pushable;
+
+  @override
+  Widget build(BuildContext context) {
+    // 文字列とボタンデザインをStackする
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        Stack(alignment: Alignment.center, children: [
+          Container(
+            height: 66,
+            width: 286,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: kTitleColor,
+            ),
+          ),
+          pushable
+              ? Container()
+              : Container(
+                  height: 66,
+                  width: 286,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Color(0xF403F4C).withOpacity(0.4)),
+                )
+        ]),
+        Text(
+          "$text",
+          style: TextStyle(
+            fontSize: 30,
+            color: kFontColor,
+          ),
+        ),
+      ],
+    );
+  }
+}

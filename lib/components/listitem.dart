31c42875c9449cabecf6f820987b13706f3f3057
櫻井 雛羽 <2200496@ecc.ac.jp>
2022-11-07 10:07:17 +0900

    ✨リストの要素

---
+++ lib/components/listitem.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:piyomiru_application/constants.dart';
+
+class Listitem extends StatelessWidget {
+  const Listitem({
+    Key? key,
+    required this.image,
+    required this.name,
+    required this.datetime,
+  }) : super(key: key);
+
+  final String image;
+  final String name;
+  final String datetime;
+
+  @override
+  Widget build(BuildContext context) {
+    double deviceW = MediaQuery.of(context).size.width;
+    double deviceH = MediaQuery.of(context).size.height;
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        Container(
+          height: deviceH * 0.12,
+          width: deviceW * 0.87,
+          decoration: BoxDecoration(
+              color: Color(0XFFFFFFFF),
+              border: Border.all(
+                width: 1.5,
+                color: kSubBackgroundColor,
+              ),
+              borderRadius: BorderRadius.circular(10)),
+        ),
+        Row(
+          children: [
+            const SizedBox(width: 36),
+            Stack(
+              alignment: Alignment.center,
+              children: [
+                Container(
+                  height: 70,
+                  width: 70,
+                  decoration: BoxDecoration(
+                      color: Color(0XFFD9D9D9),
+                      borderRadius: BorderRadius.circular(10)),
+                ),
+                Image.asset('assets/images/$image'),
+              ],
+            ),
+            SizedBox(width: 12),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "$name さん",
+                  style: const TextStyle(
+                    fontSize: 24,
+                    color: kFontColor,
+                  ),
+                ),
+                SizedBox(
+                  height: 6,
+                ),
+                Text(
+                  "$datetime",
+                  style: const TextStyle(
+                    fontSize: 15,
+                    color: kFontColor,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        )
+      ],
+    );
+  }
+}

7cf130561a4ea096b5448452cda77c8bbc530e33
櫻井 雛羽 <2200496@ecc.ac.jp>
2022-11-07 17:45:25 +0900

    ✨運転開始確認モーダル

---
+++ lib/screens/home/start_drive/start_drive_modal.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+import 'package:piyomiru_application/constants.dart';
+import 'package:piyomiru_application/screens/home/start_drive/driving_screen.dart';
+
+class StartDriveModal extends StatelessWidget {
+  StartDriveModal({
+    Key? key,
+    required this.name,
+  }) : super(key: key);
+
+  final String name;
+  @override
+  Widget build(BuildContext context) {
+    double deviceW = MediaQuery.of(context).size.width;
+    double deviceH = MediaQuery.of(context).size.height;
+
+    return Dialog(
+      alignment: Alignment.center,
+      insetPadding: const EdgeInsets.only(
+        bottom: 280,
+        top: 280,
+        left: 24,
+        right: 24,
+      ),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          Container(
+            height: deviceH,
+            width: deviceW,
+            decoration: BoxDecoration(
+              color: Color(0XFFFFFFFF),
+              borderRadius: BorderRadius.circular(20),
+              border: Border.all(width: 3, color: kSubBackgroundColor),
+            ),
+          ),
+          Column(
+            //crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(height: 38),
+              const Text(
+                "本当に運転を開始しますか？",
+                style: TextStyle(
+                  fontSize: 22,
+                  color: kFontColor,
+                ),
+              ),
+              SizedBox(height: 42),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    child: Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        Container(
+                          height: 50,
+                          width: 120,
+                          decoration: BoxDecoration(
+                            color: kCanselColor,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        const Text(
+                          "キャンセル",
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: kFontColor,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => DrivingScreen(name: name)),
+                      );
+                    },
+                    child: Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        Container(
+                          height: 50,
+                          width: 120,
+                          decoration: BoxDecoration(
+                            color: kStartColor,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        const Text(
+                          "開始",
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Color(0XFFFFFFFF),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}

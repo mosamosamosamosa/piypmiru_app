04b4f988895e0fe71058433233c3c3e6aafa7fd4
櫻井 雛羽 <2200496@ecc.ac.jp>
2022-11-07 10:07:59 +0900

    🗃データベース(仮)

--- lib/data/database.dart
+++ lib/data/database.dart
@@ -54,3 +54,70 @@ List<Operations> operations_list = [
   Operations(id: 3, start: 0, end: 1, timestamp: DateTime.now(), bus_id: 3),
   Operations(id: 4, start: 0, end: 1, timestamp: DateTime.now(), bus_id: 4),
 ];
+
+class Passengers {
+  final String name, image;
+  final int id, operation_id, user_id;
+  final DateTime timestamp;
+  final bool status;
+
+  Passengers(
+      {required this.name,
+      required this.image,
+      required this.id,
+      required this.timestamp,
+      required this.operation_id,
+      required this.user_id,
+      required this.status});
+}
+
+List<Passengers> passengers_list = [
+  Passengers(
+      id: 1,
+      name: "ぴよ ぴよこ",
+      image: "piyo_piyoko.png",
+      operation_id: 1,
+      user_id: 1,
+      timestamp: DateTime.now(),
+      status: true),
+  Passengers(
+      id: 2,
+      name: "ぴよ ぴよ太郎",
+      image: "piyo_piyotaro.png",
+      operation_id: 1,
+      user_id: 1,
+      timestamp: DateTime.now(),
+      status: true),
+  Passengers(
+      id: 3,
+      name: "ぴよ ぴよみ",
+      image: "piyo_piyomi.png",
+      operation_id: 1,
+      user_id: 1,
+      timestamp: DateTime.now(),
+      status: true),
+  Passengers(
+      id: 4,
+      name: "みる みるこ",
+      image: "miru_miruko.png",
+      operation_id: 1,
+      user_id: 1,
+      timestamp: DateTime.now(),
+      status: true),
+  Passengers(
+      id: 5,
+      name: "みる みる太郎",
+      image: "miru_mirutaro.png",
+      operation_id: 1,
+      user_id: 1,
+      timestamp: DateTime.now(),
+      status: true),
+  Passengers(
+      id: 6,
+      name: "みる みるみ",
+      image: "miru_mirumi.png",
+      operation_id: 1,
+      user_id: 1,
+      timestamp: DateTime.now(),
+      status: true),
+];

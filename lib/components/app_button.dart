28e2bfbdc94771561ffabcfbc9b9c4887f68b4b7
櫻井 雛羽 <2200496@ecc.ac.jp>
2022-11-08 10:05:53 +0900

    🩹デザイン修正

--- lib/components/app_button.dart
+++ lib/components/app_button.dart
@@ -6,42 +6,62 @@ class AppButton extends StatelessWidget {
     Key? key,
     required this.text,
     required this.start,
+    required this.pushable,
   }) : super(key: key);
 
   final String text;
   final bool start;
+  final bool pushable;
   @override
   Widget build(BuildContext context) {
+    double deviceW = MediaQuery.of(context).size.width;
+    double deviceH = MediaQuery.of(context).size.height;
     // 文字列とボタンデザインをStackする
     return Stack(
-      alignment: Alignment.center,
+      alignment: Alignment.bottomCenter,
       children: [
         // ボタンデザインContainer
-        Container(
-          width: 240,
-          height: 75,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(17),
-            boxShadow: [
-              // ボタン下
-              BoxShadow(
-                color: start == true
-                    ? const Color(0xFFDC3C14)
-                    : const Color(0xFF76C8D9),
-                offset: const Offset(0, 15),
-              ),
+        Stack(
+          alignment: Alignment.bottomCenter,
+          children: [
+            Container(
+              width: 240,
+              height: 75,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(17),
+                boxShadow: [
+                  // ボタン下
+                  BoxShadow(
+                    color: start == true
+                        ? const Color(0xFFDC3C14)
+                        : const Color(0xFF76C8D9),
+                    offset: const Offset(0, 13),
+                  ),
 
-              // ボタン上
-              BoxShadow(
-                color: start == true ? kStartColor : const Color(0xFF94EDFF),
-                //blurRadius: 0,
-                offset: const Offset(0, 4),
+                  // ボタン上
+                  BoxShadow(
+                    color:
+                        start == true ? kStartColor : const Color(0xFF94EDFF),
+                    //blurRadius: 0,
+                    offset: const Offset(0, 0),
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+            pushable
+                ? Container()
+                : Container(
+                    alignment: Alignment.bottomCenter,
+                    width: 240,
+                    height: 75,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(17),
+                        color: Color(0xF403F4C).withOpacity(0.4)),
+                  ),
+          ],
         ),
         Container(
-          margin: const EdgeInsets.only(top: 5),
+          margin: EdgeInsets.only(bottom: deviceH * 0.025),
           child: Text(
             text,
             style: TextStyle(
@@ -50,7 +70,6 @@ class AppButton extends StatelessWidget {
             ),
           ),
         ),
-        //Image.asset('assets/images/hiyoko_beginer.png')
       ],
     );
   }

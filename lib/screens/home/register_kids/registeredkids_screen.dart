e60eec2bd5f3e5be2206f05262ea22942c25515f
櫻井 雛羽 <2200496@ecc.ac.jp>
2022-11-15 14:14:30 +0900

    🩹文言変更

--- lib/screens/home/register_kids/registeredkids_screen.dart
+++ lib/screens/home/register_kids/registeredkids_screen.dart
@@ -6,7 +6,7 @@ import 'package:piyomiru_application/constants.dart';
 import 'package:piyomiru_application/data/database.dart';
 import 'package:intl/intl.dart';
 import 'package:piyomiru_application/screens/home/register_kids/addlist_modal.dart';
-import 'package:piyomiru_application/screens/home/register_kids/editlist_screen.dart';
+import 'package:piyomiru_application/screens/home/register_kids/delete_modal.dart';
 
 class RegisterkidsScreen extends StatefulWidget {
   const RegisterkidsScreen({
@@ -24,7 +24,6 @@ class _RegisterkidsScreenState extends State<RegisterkidsScreen> {
     double deviceW = MediaQuery.of(context).size.width;
     double deviceH = MediaQuery.of(context).size.height;
 
-    String text = "編集";
     bool editable = false;
 
     DateFormat outputFormat = DateFormat('yyyy/MM/dd H:m');
@@ -32,44 +31,31 @@ class _RegisterkidsScreenState extends State<RegisterkidsScreen> {
     return Scaffold(
       backgroundColor: kBackgroundColor,
       appBar: AppBar(
-          iconTheme: IconThemeData(color: kTitleColor),
-          toolbarHeight: deviceH * 0.1,
-          centerTitle: true,
-          automaticallyImplyLeading: false,
-          leading: GestureDetector(
-              onTap: () {
-                Navigator.pop(context);
-              },
-              child: Image.asset('assets/images/backmark.png')),
-          title: const Text(
-            "園児一覧",
-            style: TextStyle(
-              color: kFontColor,
-              fontSize: 26,
-            ),
+        iconTheme: IconThemeData(color: kTitleColor),
+        toolbarHeight: deviceH * 0.1,
+        centerTitle: true,
+        automaticallyImplyLeading: false,
+        leading: GestureDetector(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            child: Image.asset('assets/images/backmark.png')),
+        title: const Text(
+          "園児一覧",
+          style: TextStyle(
+            color: kFontColor,
+            fontSize: 26,
           ),
-          backgroundColor: kSubBackgroundColor,
-          elevation: 0.0,
-          actions: [
-            GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => EditlistScreen()),
-                );
-              },
-              child: ActionButton(
-                text: text,
-                img: "hiyoko_pencil.png",
-              ),
-            ),
-          ]),
+        ),
+        backgroundColor: kSubBackgroundColor,
+        elevation: 0.0,
+      ),
       body: ListView.builder(
         padding: EdgeInsets.only(top: deviceH * 0.012),
-        itemCount: passengers_list.length + 1,
+        itemCount: users_list.length + 1,
         shrinkWrap: true,
         itemBuilder: (BuildContext context, index) {
-          if (index == passengers_list.length) {
+          if (index == users_list.length) {
             return GestureDetector(
                 onTap: () async {
                   String name = await showDialog(
@@ -85,11 +71,22 @@ class _RegisterkidsScreenState extends State<RegisterkidsScreen> {
                     child: const Addlistitem()));
           }
 
-          return Listitem(
-              editable: editable,
-              image: passengers_list[index].image,
-              name: passengers_list[index].name,
-              datetime: outputFormat.format(DateTime.now()));
+          return GestureDetector(
+            onTap: () {
+              showDialog(
+                barrierDismissible: false,
+                context: context,
+                builder: (BuildContext context) => DeleteModal(
+                    name: users_list[index].name,
+                    image: users_list[index].image),
+              );
+            },
+            child: Listitem(
+                editable: editable,
+                image: users_list[index].image,
+                name: users_list[index].name,
+                datetime: outputFormat.format(DateTime.now())),
+          );
         },
       ),
     );

e6d67bf190f3a03a16d3aa5bfd2f30e53cd7b677
櫻井 雛羽 <2200496@ecc.ac.jp>
2022-11-22 12:27:31 +0900

    🍱カラーコード追加

--- lib/constants.dart
+++ lib/constants.dart
@@ -12,3 +12,4 @@ const kCanselColor = Color(0XFFE3EEF9);
 const kEndColor = Color(0XFF90D7EC);
 const kStartColor = Color(0XFFED6D47);
 const kBackgroundColors = [Color(0XFFA0D8EF), Color(0xFFFFFAE3)];
+const kStopBackgroundColors = [Color(0XFF00679E), Color(0xFFFFFAE3)];

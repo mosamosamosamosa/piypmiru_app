cb6ea3f6a1ef25088c206db096208d8c3268c8d4
櫻井 雛羽 <2200496@ecc.ac.jp>
2022-11-22 16:23:35 +0900

    🚧データベース関連のファイル

--- lib/screens/login/login_screeen.dart
+++ lib/screens/login/login_screeen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:piyomiru_application/components/nomal_button.dart';
 import 'package:piyomiru_application/constants.dart';
+import 'package:piyomiru_application/providers/login_provider.dart';
+import 'package:provider/provider.dart';
 
 class LoginScreen extends StatefulWidget {
   LoginScreen({Key? key}) : super(key: key);
@@ -19,164 +21,169 @@ class _LoginScreenState extends State<LoginScreen> {
     double deviceH = MediaQuery.of(context).size.height;
 
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      backgroundColor: kSubBackgroundColor,
-      body: Stack(
-        children: [
-          Column(
+        resizeToAvoidBottomInset: false,
+        backgroundColor: kSubBackgroundColor,
+        body: Consumer<LoginProvider>(builder: (context, loginProvider, _) {
+          return Stack(
             children: [
-              Container(
-                margin: EdgeInsets.only(top: deviceH * 1 / 6),
-                alignment: Alignment.center,
-                child: const Text(
-                  "PiyoMiru",
-                  style: TextStyle(
-                      fontSize: 70,
-                      fontWeight: FontWeight.bold,
-                      color: kTitleColor,
-                      fontFamily: 'Rajdhani-B'),
-                ),
-              ),
-              SizedBox(height: deviceH * 0.06),
-              Stack(
+              Column(
                 children: [
                   Container(
-                    height: deviceH * 0.10,
-                    width: deviceW * 0.78,
-                    decoration: BoxDecoration(
-                      color: kInputColor,
-                      borderRadius: BorderRadius.circular(20),
-                      border: Border.all(width: 3, color: kSubBackgroundColor),
-                    ),
-                    child: Container(
-                      alignment: Alignment.bottomCenter,
-                      child: TextField(
-                        //controller: controllerN,
-                        style: const TextStyle(
-                          fontSize: 24,
-                          color: kFontColor,
-                        ),
-                        cursorColor: kFontColor,
-                        decoration: const InputDecoration(
-                          contentPadding: EdgeInsets.only(left: 24),
-                          enabledBorder: InputBorder.none,
-                          focusedBorder: InputBorder.none,
-                        ),
-                        onChanged: (value) {
-                          if (value.isNotEmpty) {
-                            setState(() {
-                              pushN = true;
-                            });
-                          } else {
-                            setState(() {
-                              pushN = false;
-                            });
-                          }
-                        },
-                      ),
-                    ),
-                  ),
-                  const Positioned(
-                    top: 11,
-                    left: 20,
-                    child: Text(
-                      "ユーザーネーム",
+                    margin: EdgeInsets.only(top: deviceH * 1 / 6),
+                    alignment: Alignment.center,
+                    child: const Text(
+                      "PiyoMiru",
                       style: TextStyle(
-                          fontSize: 12,
-                          color: kFontColor,
-                          fontFamily: 'KiwiMaru-L'),
+                          fontSize: 70,
+                          fontWeight: FontWeight.bold,
+                          color: kTitleColor,
+                          fontFamily: 'Rajdhani-B'),
                     ),
                   ),
-                ],
-              ),
-              SizedBox(height: deviceH * 0.03),
-              Stack(
-                children: [
-                  Container(
-                    height: deviceH * 0.10,
-                    width: deviceW * 0.78,
-                    decoration: BoxDecoration(
-                      color: kInputColor,
-                      borderRadius: BorderRadius.circular(20),
-                      border: Border.all(width: 3, color: kSubBackgroundColor),
-                    ),
-                    child: Container(
-                      alignment: Alignment.bottomCenter,
-                      child: TextField(
-                        //controller: controllerP,
-                        obscureText: true,
-                        style: const TextStyle(
-                          fontSize: 24,
-                          color: kFontColor,
-                          fontFamily: 'KiwiMaru-R',
+                  SizedBox(height: deviceH * 0.06),
+                  Stack(
+                    children: [
+                      Container(
+                        height: deviceH * 0.10,
+                        width: deviceW * 0.78,
+                        decoration: BoxDecoration(
+                          color: kInputColor,
+                          borderRadius: BorderRadius.circular(20),
+                          border:
+                              Border.all(width: 3, color: kSubBackgroundColor),
                         ),
-                        cursorColor: kFontColor,
-                        decoration: const InputDecoration(
-                          contentPadding: EdgeInsets.only(left: 24),
-                          enabledBorder: InputBorder.none,
-                          focusedBorder: InputBorder.none,
+                        child: Container(
+                          alignment: Alignment.bottomCenter,
+                          child: TextField(
+                            //controller: controllerN,
+                            style: const TextStyle(
+                              fontSize: 24,
+                              color: kFontColor,
+                            ),
+                            cursorColor: kFontColor,
+                            decoration: const InputDecoration(
+                              contentPadding: EdgeInsets.only(left: 24),
+                              enabledBorder: InputBorder.none,
+                              focusedBorder: InputBorder.none,
+                            ),
+                            onChanged: (value) {
+                              if (value.isNotEmpty) {
+                                loginProvider.username = value;
+                                setState(() {
+                                  pushN = true;
+                                });
+                              } else {
+                                setState(() {
+                                  pushN = false;
+                                });
+                              }
+                            },
+                          ),
                         ),
-                        onChanged: (value) {
-                          if (value.isNotEmpty) {
-                            setState(() {
-                              pushP = true;
-                            });
-                          } else {
-                            setState(() {
-                              pushP = false;
-                            });
-                          }
+                      ),
+                      const Positioned(
+                        top: 11,
+                        left: 20,
+                        child: Text(
+                          "ユーザーネーム",
+                          style: TextStyle(
+                              fontSize: 12,
+                              color: kFontColor,
+                              fontFamily: 'KiwiMaru-L'),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: deviceH * 0.03),
+                  Stack(
+                    children: [
+                      Container(
+                        height: deviceH * 0.10,
+                        width: deviceW * 0.78,
+                        decoration: BoxDecoration(
+                          color: kInputColor,
+                          borderRadius: BorderRadius.circular(20),
+                          border:
+                              Border.all(width: 3, color: kSubBackgroundColor),
+                        ),
+                        child: Container(
+                          alignment: Alignment.bottomCenter,
+                          child: TextField(
+                            //controller: controllerP,
+                            obscureText: true,
+                            style: const TextStyle(
+                              fontSize: 24,
+                              color: kFontColor,
+                              fontFamily: 'KiwiMaru-R',
+                            ),
+                            cursorColor: kFontColor,
+                            decoration: const InputDecoration(
+                              contentPadding: EdgeInsets.only(left: 24),
+                              enabledBorder: InputBorder.none,
+                              focusedBorder: InputBorder.none,
+                            ),
+                            onChanged: (value) {
+                              if (value.isNotEmpty) {
+                                loginProvider.password = value;
+                                setState(() {
+                                  pushP = true;
+                                });
+                              } else {
+                                setState(() {
+                                  pushP = false;
+                                });
+                              }
 
-                          ;
-                        },
+                              ;
+                            },
+                          ),
+                        ),
                       ),
-                    ),
-                  ),
-                  const Positioned(
-                    top: 11,
-                    left: 20,
-                    child: Text(
-                      "パスワード",
-                      style: TextStyle(
-                          fontSize: 12,
-                          color: kFontColor,
-                          fontFamily: 'KiwiMaru-L'),
-                    ),
+                      const Positioned(
+                        top: 11,
+                        left: 20,
+                        child: Text(
+                          "パスワード",
+                          style: TextStyle(
+                              fontSize: 12,
+                              color: kFontColor,
+                              fontFamily: 'KiwiMaru-L'),
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
+              pushN && pushP
+                  ? Positioned(
+                      bottom: deviceH * 1 / 8,
+                      left: 0,
+                      right: 0,
+                      child: GestureDetector(
+                          onTap: () {
+                            Navigator.of(context).pushReplacementNamed("/home");
+                          },
+                          child: NomalButton(
+                            text: "ログイン",
+                            pushable: true,
+                          )),
+                    )
+                  : Positioned(
+                      bottom: deviceH * 1 / 8,
+                      left: 0,
+                      right: 0,
+                      child: GestureDetector(
+                          onTap: () {
+                            print("P=$pushP");
+                            print("N=$pushN");
+                          },
+                          child: NomalButton(
+                            text: "ログイン",
+                            pushable: false,
+                          )),
+                    )
             ],
-          ),
-          pushN && pushP
-              ? Positioned(
-                  bottom: deviceH * 1 / 8,
-                  left: 0,
-                  right: 0,
-                  child: GestureDetector(
-                      onTap: () {
-                        Navigator.of(context).pushReplacementNamed("/home");
-                      },
-                      child: NomalButton(
-                        text: "ログイン",
-                        pushable: true,
-                      )),
-                )
-              : Positioned(
-                  bottom: deviceH * 1 / 8,
-                  left: 0,
-                  right: 0,
-                  child: GestureDetector(
-                      onTap: () {
-                        print("P=$pushP");
-                        print("N=$pushN");
-                      },
-                      child: NomalButton(
-                        text: "ログイン",
-                        pushable: false,
-                      )),
-                )
-        ],
-      ),
-    );
+          );
+        }));
   }
 }

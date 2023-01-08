5235af63004bce63f436e2d8e0da70f558026d65
櫻井 雛羽 <2200496@ecc.ac.jp>
2023-01-08 23:16:07 +0900

    🩹ひよこ追加

--- lib/screens/driver/nfc/nfc_success_modal.dart
+++ lib/screens/driver/nfc/nfc_success_modal.dart
@@ -9,65 +9,73 @@ class NfcSuccessModal extends StatelessWidget {
     double deviceW = MediaQuery.of(context).size.width;
     double deviceH = MediaQuery.of(context).size.height;
 
-    return Dialog(
-      alignment: Alignment.center,
-      insetPadding: const EdgeInsets.only(
-        bottom: 280,
-        top: 280,
-        left: 24,
-        right: 24,
-      ),
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(20),
-      ),
-      child: Stack(
-        alignment: Alignment.center,
-        children: [
-          Container(
-            height: deviceH,
-            width: deviceW,
-            decoration: BoxDecoration(
-              color: Color(0XFFFFFFFF),
-              borderRadius: BorderRadius.circular(20),
-              border: Border.all(width: 3, color: kSubBackgroundColor),
-            ),
+    return Stack(
+      children: [
+        Positioned(
+            top: 210,
+            left: 35,
+            child: Image.asset('assets/images/hiyoko_anzen.png')),
+        Dialog(
+          alignment: Alignment.center,
+          insetPadding: const EdgeInsets.only(
+            bottom: 280,
+            top: 280,
+            left: 24,
+            right: 24,
           ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Stack(
+            alignment: Alignment.center,
             children: [
-              const Text(
-                "〇〇　〇〇さん\n乗車完了！",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 24,
-                  color: kFontColor,
+              Container(
+                height: deviceH,
+                width: deviceW,
+                decoration: BoxDecoration(
+                  color: Color(0XFFFFFFFF),
+                  borderRadius: BorderRadius.circular(20),
+                  border: Border.all(width: 3, color: kSubBackgroundColor),
                 ),
               ),
-              GestureDetector(
-                onTap: () {
-                  //キャンセル処理
-                  Navigator.pop(context);
-                },
-                child: Stack(
-                  alignment: Alignment.center,
-                  children: [
-                    Container(
-                      height: 50,
-                      width: 120,
-                      decoration: BoxDecoration(
-                        color: kCanselColor,
-                        borderRadius: BorderRadius.circular(10),
-                      ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  const Text(
+                    "〇〇　〇〇さん\n乗車完了！",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 24,
+                      color: kFontColor,
                     ),
-                    const Text("閉じる",
-                        style: TextStyle(fontSize: 18, color: kFontColor)),
-                  ],
-                ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      //キャンセル処理
+                      Navigator.pop(context);
+                    },
+                    child: Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        Container(
+                          height: 50,
+                          width: 120,
+                          decoration: BoxDecoration(
+                            color: kCanselColor,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        const Text("閉じる",
+                            style: TextStyle(fontSize: 18, color: kFontColor)),
+                      ],
+                    ),
+                  )
+                ],
               )
             ],
-          )
-        ],
-      ),
+          ),
+        ),
+      ],
     );
   }
 }

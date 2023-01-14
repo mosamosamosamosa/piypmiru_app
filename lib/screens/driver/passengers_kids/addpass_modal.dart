ca10bacae0724edcfeeff701bd104a101656063e
櫻井 雛羽 <2200496@ecc.ac.jp>
2023-01-14 23:22:28 +0900

    ✨手動での乗客追加機能

---
+++ lib/screens/driver/passengers_kids/addpass_modal.dart
@@ -0,0 +1,154 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:piyomiru_application/api/passenger.dart';
+import 'package:piyomiru_application/api/users.dart';
+import 'package:piyomiru_application/constants.dart';
+
+class AddpassModal extends StatefulWidget {
+  AddpassModal({Key? key}) : super(key: key);
+
+  @override
+  State<AddpassModal> createState() => _AddpassModalState();
+}
+
+class _AddpassModalState extends State<AddpassModal> {
+  String name = '';
+  int userId = 0;
+  @override
+  Widget build(BuildContext context) {
+    double deviceW = MediaQuery.of(context).size.width;
+    double deviceH = MediaQuery.of(context).size.height;
+    //final controller = TextEditingController();
+
+    return SingleChildScrollView(
+      child: Dialog(
+        alignment: Alignment.center,
+        insetPadding: const EdgeInsets.only(
+          bottom: 250,
+          top: 250,
+          left: 28,
+          right: 28,
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Container(
+          height: 260,
+          width: 350,
+          decoration: BoxDecoration(
+            color: Color(0XFFFFFFFF),
+            borderRadius: BorderRadius.circular(20),
+            border: Border.all(width: 3, color: kSubBackgroundColor),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              //SizedBox(height: 30),
+              const Text(
+                "名前を入力してください",
+                style: TextStyle(
+                    fontSize: 22, color: kFontColor, fontFamily: 'KiwiMaru-R'),
+              ),
+
+              Container(
+                height: 50,
+                width: 250,
+                decoration: BoxDecoration(
+                  color: kInputColor,
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: TextField(
+                  //controller: controller,
+                  style: const TextStyle(
+                    fontSize: 24,
+                    color: kFontColor,
+                  ),
+                  cursorColor: kFontColor,
+                  decoration: const InputDecoration(
+                    contentPadding: EdgeInsets.only(left: 24),
+                    enabledBorder: InputBorder.none,
+                    focusedBorder: InputBorder.none,
+                  ),
+                  onChanged: (value) {
+                    setState(() {
+                      name = value;
+                    });
+                  },
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    child: Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        Container(
+                          height: 64,
+                          width: 120,
+                          decoration: BoxDecoration(
+                            color: kCanselColor,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        const Text(
+                          "キャンセル",
+                          style: TextStyle(
+                              fontSize: 18,
+                              color: kFontColor,
+                              fontFamily: 'KiwiMaru-R'),
+                        ),
+                      ],
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      //追加処理
+
+                      if (name.isNotEmpty) {
+                        var f = Users().getAllUsers(name);
+
+                        f.then((value) => {
+                              userId = value,
+                              print(userId),
+                              Passenger().postPassenger(userId, true),
+                              Navigator.pop(context)
+                            });
+                      } else {
+                        Navigator.pop(context);
+                        //print("false");
+                      }
+                    },
+                    child: Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        Container(
+                          height: 64,
+                          width: 120,
+                          decoration: BoxDecoration(
+                            color: kStartColor,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        const Text(
+                          "追加",
+                          style: TextStyle(
+                              fontSize: 18,
+                              color: Color(0XFFFFFFFF),
+                              fontFamily: 'KiwiMaru-R'),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}

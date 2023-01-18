fc1eb37b12bdc71a82f3cc43a233ba9db4000c92
櫻井 雛羽 <2200496@ecc.ac.jp>
2023-01-18 14:03:36 +0900

    ✨乗客API連携

--- lib/api/passenger.dart
+++ lib/api/passenger.dart
@@ -24,56 +24,8 @@ class Passenger {
     var response = await http.Response.fromStream(stream_response);
 
     if (response.statusCode == 200) {
-      //passengerList = jsonDecode(response.body).cast<Map<String, dynamic>>();
+      passengerList = jsonDecode(response.body).cast<Map<String, dynamic>>();
       //idList = passengerList.map((e) => e['user_id']).toList();
-
-      passengerList = [
-        {
-          "id": 7,
-          "status": true,
-          "created": "2023-01-12T15:20:05.377910+09:00",
-          "operation_id": null,
-          "user_id": {
-            "id": 1,
-            "name": "test",
-            "email": "test@gmail.com",
-            "driver": false,
-            "created": "2022-12-01T14:23:31.818852+09:00",
-            "group_id": 1,
-            "family_id": 1
-          }
-        },
-        {
-          "id": 8,
-          "status": true,
-          "created": "2023-01-12T15:20:11.333192+09:00",
-          "operation_id": null,
-          "user_id": {
-            "id": 2,
-            "name": "test3",
-            "email": "test3@gmail.com",
-            "driver": false,
-            "created": "2022-12-08T13:44:14.328146+09:00",
-            "group_id": null,
-            "family_id": null
-          }
-        },
-        {
-          "id": 9,
-          "status": false,
-          "created": "2023-01-12T15:20:05.377910+09:00",
-          "operation_id": null,
-          "user_id": {
-            "id": 3,
-            "name": "test",
-            "email": "test@gmail.com",
-            "driver": false,
-            "created": "2022-12-01T14:23:31.818852+09:00",
-            "group_id": 1,
-            "family_id": 1
-          }
-        },
-      ];
 
       //乗車中の園児のIDだけを取得
       passengerList.forEach((element) {
@@ -92,32 +44,13 @@ class Passenger {
     }
   }
 
-  //バス乗車
+  //バス乗車降車
   //NFCでスキャンしたユーザIDを受け取ってpassengerに追加
   postPassenger(int userId, bool status) async {
     var headers = {'Content-Type': 'application/json'};
     var request = http.Request('POST', Uri.parse('${Clients().url}/passenger'));
-    request.body = json
-        .encode({"status": status, "operation_id": null, "user_id": userId});
-    request.headers.addAll(headers);
-
-    http.StreamedResponse stream_response = await request.send();
-    var response = await http.Response.fromStream(stream_response);
-
-    if (response.statusCode == 200) {
-      print(response.body);
-      print("成功");
-    } else {
-      print(response.reasonPhrase);
-    }
-  }
-
-  //降車
-  compPassenger(int userId, bool status) async {
-    var headers = {'Content-Type': 'application/json'};
-    var request = http.Request('POST', Uri.parse('${Clients().url}/passenger'));
-    request.body = json
-        .encode({"status": status, "operation_id": null, "user_id": userId});
+    request.body =
+        json.encode({"status": status, "operation": 1, "user": userId});
     request.headers.addAll(headers);
 
     http.StreamedResponse stream_response = await request.send();

ecdb014709d9cb341d968c49e57dbcf1e95f9867
櫻井 雛羽 <2200496@ecc.ac.jp>
2023-01-24 13:41:58 +0900

    ✨passenger_id取得

--- lib/api/passenger.dart
+++ lib/api/passenger.dart
@@ -31,16 +31,16 @@ class Passenger {
       passengerList.forEach((element) {
         if (element['status']) {
           idList.add(element['user']);
+          print(element['status']);
         }
       });
-      print(passengerList[0]['status']);
 
       //mapのリスト
       return idList;
     } else {
       debugPrint(response.reasonPhrase);
 
-      return "失敗";
+      return idList;
     }
   }
 
@@ -50,7 +50,7 @@ class Passenger {
     var headers = {'Content-Type': 'application/json'};
     var request = http.Request('POST', Uri.parse('${Clients().url}/passenger'));
     request.body =
-        json.encode({"status": true, "operation_id": 4, "user_id": userId});
+        json.encode({"status": true, "operation_id": 3, "user_id": userId});
     request.headers.addAll(headers);
 
     http.StreamedResponse stream_response = await request.send();
@@ -69,7 +69,7 @@ class Passenger {
     var request =
         http.Request('PUT', Uri.parse('${Clients().url}/passenger/$passId'));
     request.body =
-        json.encode({"status": false, "operation_id": 4, "user_id": userId});
+        json.encode({"status": false, "operation_id": 3, "user_id": userId});
     request.headers.addAll(headers);
 
     http.StreamedResponse response = await request.send();
@@ -116,6 +116,55 @@ class Passenger {
       debugPrint(response.reasonPhrase);
 
       return "失敗";
+    }
+  }
+
+  //userIdからPassengerId求める
+  getpassIdPassenger(int userId) async {
+    List<int> idList = [];
+    var passId;
+    var request_all_passenger =
+        http.Request('GET', Uri.parse('${Clients().url}/passenger'));
+    request_all_passenger.body = '''''';
+
+    http.StreamedResponse stream_response = await request_all_passenger.send();
+    var response = await http.Response.fromStream(stream_response);
+
+    if (response.statusCode == 200) {
+      passengerList = jsonDecode(response.body).cast<Map<String, dynamic>>();
+      //idList = passengerList.map((e) => e['user_id']).toList();
+
+      // //乗車中の園児のIDだけを取得
+      // passengerList.forEach((element) {
+      //   if (element['status']) {
+      //     idList.add(element['user']['id']);
+      //   }
+      // });
+
+      // for (int i = 0; i <= idList.length; i++) {
+      //   if (idList[i] == userId) {
+      //     passId = passengerList[i]['id'];
+      //     break;
+      //   }
+      // }
+
+      passengerList.forEach((element) {
+        idList.add(element['user']['id']);
+      });
+
+      for (int i = 0; i <= idList.length; i++) {
+        if (idList[i] == userId) {
+          passId = passengerList[i]['id'];
+          break;
+        }
+      }
+
+      //mapのリスト
+      return passId;
+    } else {
+      debugPrint(response.reasonPhrase);
+
+      return 0;
     }
   }
 }

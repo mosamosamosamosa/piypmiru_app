06b9dd4f1b3e156be1bfd2b83d02cf78db1ef734
櫻井 雛羽 <2200496@ecc.ac.jp>
2023-01-30 00:03:21 +0900

    ✨NFC書き込み処理

--- lib/screens/parent/addkids_modal.dart
+++ lib/screens/parent/addkids_modal.dart
@@ -1,11 +1,21 @@
+import 'dart:async';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:nfc_in_flutter/nfc_in_flutter.dart';
 import 'package:piyomiru_application/api/passenger.dart';
 import 'package:piyomiru_application/api/users.dart';
 import 'package:piyomiru_application/constants.dart';
 import 'package:piyomiru_application/screens/parent/nfc_scan_modal.dart';
+import 'package:piyomiru_application/screens/parent/write_nfc_in_flutter.dart';
 
 //////////package:piyomiru_application/screens/parent/family_lsit.dartから呼ばれています//////////
+
+class RecordEditor {
+  final TextEditingController mediaTypeController = TextEditingController();
+  final TextEditingController payloadController = TextEditingController();
+}
 
 class AddkidsModal extends StatefulWidget {
   AddkidsModal({Key? key}) : super(key: key);
@@ -17,6 +27,56 @@ class AddkidsModal extends StatefulWidget {
 class _AddkidsModalState extends State<AddkidsModal> {
   String name = '';
   int userId = 0;
+  StreamSubscription<NDEFMessage>? _stream;
+  List<RecordEditor> _records = [];
+  bool _hasClosedWriteDialog = false;
+
+  void initState() {
+    setState(() {
+      _records.add(RecordEditor());
+    });
+    super.initState();
+  }
+
+  void _write(BuildContext context) async {
+    List<NDEFRecord> records = _records.map((record) {
+      print("メディアの内容:${record.mediaTypeController.text}");
+      return NDEFRecord.type(
+        record.mediaTypeController.text,
+        record.payloadController.text,
+      );
+    }).toList();
+
+    print("レコードの内容：$records");
+    NDEFMessage message = NDEFMessage.withRecords(records);
+
+    // Show dialog on Android (iOS has it's own one)
+    if (Platform.isAndroid) {
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: const Text("Scan the tag you want to write to"),
+          actions: <Widget>[
+            TextButton(
+              child: const Text("Cancel"),
+              onPressed: () {
+                _hasClosedWriteDialog = true;
+                _stream?.cancel();
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      );
+    }
+
+    // Write to the first tag scanned
+    await NFC.writeNDEF(message).first;
+    if (!_hasClosedWriteDialog) {
+      Navigator.pop(context);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     double deviceW = MediaQuery.of(context).size.width;
@@ -59,24 +119,31 @@ class _AddkidsModalState extends State<AddkidsModal> {
                   color: kInputColor,
                   borderRadius: BorderRadius.circular(5),
                 ),
-                child: TextField(
-                  //controller: controller,
-                  style: const TextStyle(
-                    fontSize: 24,
-                    color: kFontColor,
-                  ),
-                  cursorColor: kFontColor,
-                  decoration: const InputDecoration(
-                    contentPadding: EdgeInsets.only(left: 24),
-                    enabledBorder: InputBorder.none,
-                    focusedBorder: InputBorder.none,
-                  ),
-                  onChanged: (value) {
-                    setState(() {
-                      name = value;
-                    });
-                  },
-                ),
+                child: ListView(children: <Widget>[
+                  for (var record in _records)
+                    Column(
+                      children: [
+                        TextField(
+                          onChanged: (value) {
+                            setState(() {
+                              name = value;
+                            });
+                          },
+                          controller: record.mediaTypeController,
+                          style: const TextStyle(
+                            fontSize: 24,
+                            color: kFontColor,
+                          ),
+                          cursorColor: kFontColor,
+                          decoration: const InputDecoration(
+                            contentPadding: EdgeInsets.only(left: 24),
+                            enabledBorder: InputBorder.none,
+                            focusedBorder: InputBorder.none,
+                          ),
+                        ),
+                      ],
+                    )
+                ]),
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
@@ -111,15 +178,17 @@ class _AddkidsModalState extends State<AddkidsModal> {
                       //追加処理
 
                       if (name.isNotEmpty) {
+                        _write(context);
+
                         // Navigator.of(context)
                         //     .push(MaterialPageRoute(builder: (context) {
-                        //   return const NfcScanModal();
+                        //   return WriteExampleScreen();
                         // }));
 
-                        showDialog(
-                            barrierDismissible: false,
-                            context: context,
-                            builder: (BuildContext context) => NfcScanModal());
+                        // showDialog(
+                        //     barrierDismissible: false,
+                        //     context: context,
+                        //     builder: (BuildContext context) => NfcScanModal());
                       } else {
                         Navigator.pop(context);
                         //print("false");

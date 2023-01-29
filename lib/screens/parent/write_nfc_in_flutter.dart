5a6c8424e93361b47ee666d1605e269844ace5a2
櫻井 雛羽 <2200496@ecc.ac.jp>
2023-01-30 00:04:23 +0900

    🗑NFC書き込みテスト

---
+++ lib/screens/parent/write_nfc_in_flutter.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+import 'package:nfc_in_flutter/nfc_in_flutter.dart';
+import 'dart:async';
+import 'dart:io';
+
+class RecordEditor {
+  final TextEditingController mediaTypeController = TextEditingController();
+  final TextEditingController payloadController = TextEditingController();
+}
+
+class WriteExampleScreen extends StatefulWidget {
+  @override
+  _WriteExampleScreenState createState() => _WriteExampleScreenState();
+}
+
+class _WriteExampleScreenState extends State<WriteExampleScreen> {
+  StreamSubscription<NDEFMessage>? _stream;
+  List<RecordEditor> _records = [];
+  bool _hasClosedWriteDialog = false;
+  String name = '';
+
+  void initState() {
+    setState(() {
+      _records.add(RecordEditor());
+    });
+    super.initState();
+  }
+
+  void _write(BuildContext context) async {
+    List<NDEFRecord> records = _records.map((record) {
+      print("メディアの内容:${record.mediaTypeController.text}");
+      return NDEFRecord.type(
+        record.mediaTypeController.text,
+        record.payloadController.text,
+      );
+    }).toList();
+
+    print("レコードの内容：$records");
+    NDEFMessage message = NDEFMessage.withRecords(records);
+
+    // Show dialog on Android (iOS has it's own one)
+    if (Platform.isAndroid) {
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: const Text("Scan the tag you want to write to"),
+          actions: <Widget>[
+            TextButton(
+              child: const Text("Cancel"),
+              onPressed: () {
+                _hasClosedWriteDialog = true;
+                _stream?.cancel();
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      );
+    }
+
+    // Write to the first tag scanned
+    await NFC.writeNDEF(message).first;
+    if (!_hasClosedWriteDialog) {
+      Navigator.pop(context);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Write NFC example"),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(20),
+        children: <Widget>[
+          // Center(
+          //   child: OutlinedButton(
+          //     child: const Text("Add record"),
+          //     onPressed: _addRecord,
+          //   ),
+          // ),
+          for (var record in _records)
+            Padding(
+              padding: const EdgeInsets.only(bottom: 30),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text("Record", style: Theme.of(context).textTheme.bodyText1),
+                  TextFormField(
+                    controller: record.mediaTypeController,
+                    decoration: InputDecoration(
+                      hintText: "Media type",
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          Center(
+            child: ElevatedButton(
+              child: const Text("Write to tag"),
+              onPressed: _records.length > 0 ? () => _write(context) : null,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}

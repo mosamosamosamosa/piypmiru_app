dd9483972eb7c15f05a1d265100c8f78628fc2c0
櫻井 雛羽 <2200496@ecc.ac.jp>
2023-01-30 21:02:49 +0900

    ✨お子様のユーザ登録時のNFCスキャンモーダル

--- lib/screens/parent/nfc_scan_modal.dart
+++ lib/screens/parent/nfc_scan_modal.dart
@@ -37,181 +37,174 @@ class _NfcScanModalState extends State<NfcScanModal> {
     double deviceH = MediaQuery.of(context).size.height;
 
     return SingleChildScrollView(
-      child: Stack(
-        children: [
-          // Positioned(
-          //     top: 210,
-          //     left: 35,
-          //     child: Image.asset('assets/images/hiyoko_anzen.png')),
-          Dialog(
-            //////////<お子様の名前を入力してください>のダイアログ//////////
-            alignment: Alignment.center,
-            insetPadding: const EdgeInsets.only(
-              bottom: 250,
-              top: 250,
-              left: 28,
-              right: 28,
+      child: Dialog(
+        //////////<お子様の名前を入力してください>のダイアログ//////////
+
+        alignment: Alignment.center,
+        insetPadding: const EdgeInsets.only(
+          bottom: 250,
+          top: 250,
+          left: 28,
+          right: 28,
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Container(
+              height: 260,
+              width: 370,
+              decoration: BoxDecoration(
+                color: Color(0XFFFFFFFF),
+                borderRadius: BorderRadius.circular(20),
+                border: Border.all(width: 3, color: kSubBackgroundColor),
+              ),
             ),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(20),
-            ),
-            child: Stack(
-              alignment: Alignment.center,
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Container(
-                  height: 260,
-                  width: 350,
-                  decoration: BoxDecoration(
-                    color: Color(0XFFFFFFFF),
-                    borderRadius: BorderRadius.circular(20),
-                    border: Border.all(width: 3, color: kSubBackgroundColor),
-                  ),
-                ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    widget.success //////////スキャン成功したら表示//////////
+                widget.success //////////スキャン成功したら表示//////////
+                    ? Text(
+                        "${widget.kidName}さん登録完了！",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 24,
+                          color: kFontColor,
+                        ),
+                      )
+                    : failed //////////スキャン失敗したら表示//////////
                         ? const Text(
-                            "さん登録完了！",
+                            "読み取り失敗しました",
                             textAlign: TextAlign.center,
                             style: TextStyle(
                               fontSize: 24,
                               color: kFontColor,
                             ),
                           )
-                        : failed //////////スキャン失敗したら表示//////////
-                            ? const Text(
-                                "読み取り失敗しました",
-                                textAlign: TextAlign.center,
-                                style: TextStyle(
-                                  fontSize: 24,
-                                  color: kFontColor,
-                                ),
-                              )
-                            : Text(
-                                //////////!success&&!failedなら表示//////////
-                                "お子様のNFCカードを\nスキャンしてください",
-                                textAlign: TextAlign.center,
-                                style: TextStyle(
-                                    fontSize: 24,
-                                    color: kFontColor,
-                                    fontFamily: 'KiwiMaru-R'),
-                              ),
-                    widget.success //////////スキャン成功したら表示//////////
-                        ? Image.asset('assets/images/hiyoko_success.png')
-                        : failed //////////スキャン失敗したら表示//////////
-                            ? Image.asset('assets/images/hiyoko_batsu.png')
-
+                        : const Text(
                             //////////!success&&!failedなら表示//////////
-                            : Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
+                            "お子様のNFCカードを\nスキャンしてください",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                fontSize: 24,
+                                color: kFontColor,
+                                fontFamily: 'KiwiMaru-R'),
+                          ),
+                widget.success //////////スキャン成功したら表示//////////
+                    ? Image.asset('assets/images/hiyoko_success.png')
+                    : failed //////////スキャン失敗したら表示//////////
+                        ? Image.asset('assets/images/hiyoko_batsu.png')
+
+                        //////////!success&&!failedなら表示//////////
+                        : Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Stack(
+                                alignment: AlignmentDirectional.center,
                                 children: [
-                                  Stack(
-                                    alignment: AlignmentDirectional.center,
-                                    children: [
-                                      Container(
-                                        height: 86,
-                                        width: 86,
-                                        decoration: const BoxDecoration(
-                                          shape: BoxShape.circle,
-                                          color: kInputColor,
-                                          //color: kSubBackgroundColor,
-                                        ),
-                                      ),
-                                      Image.asset(
-                                          'assets/images/hiyoko_nfc_left.png')
-                                    ],
+                                  Container(
+                                    height: 86,
+                                    width: 86,
+                                    decoration: const BoxDecoration(
+                                      shape: BoxShape.circle,
+                                      color: kInputColor,
+                                      //color: kSubBackgroundColor,
+                                    ),
                                   ),
-                                  SizedBox(
-                                    width: 15,
-                                  ),
-                                  Image.asset('assets/images/nfc_sannkaku.png'),
-                                  SizedBox(
-                                    width: 10,
-                                  ),
-                                  Image.asset('assets/images/nfc_sannkaku.png'),
-                                  SizedBox(
-                                    width: 10,
-                                  ),
-                                  Image.asset('assets/images/nfc_sannkaku.png'),
-                                  SizedBox(
-                                    width: 15,
-                                  ),
-                                  Stack(
-                                    alignment: AlignmentDirectional.center,
-                                    children: [
-                                      Container(
-                                        height: 86,
-                                        width: 86,
-                                        decoration: const BoxDecoration(
-                                          shape: BoxShape.circle,
-                                          color: kInputColor,
-                                          //color: kSubBackgroundColor,
-                                        ),
-                                      ),
-                                      Image.asset(
-                                          'assets/images/hiyoko_nfc_right.png')
-                                    ],
-                                  ),
+                                  Image.asset(
+                                      'assets/images/hiyoko_nfc_left.png')
                                 ],
                               ),
-                    widget.success || failed //////////閉じるボタン//////////
-                        ? GestureDetector(
-                            onTap: () {
-                              Navigator.of(context).pop();
-                              Navigator.of(context).pop();
-                              Navigator.of(context).pop();
-                            },
-                            child: Stack(
-                              alignment: Alignment.center,
-                              children: [
-                                Container(
-                                  height: 50,
-                                  width: 120,
-                                  decoration: BoxDecoration(
-                                    color: kCanselColor,
-                                    borderRadius: BorderRadius.circular(10),
+                              SizedBox(
+                                width: 15,
+                              ),
+                              Image.asset('assets/images/nfc_sannkaku.png'),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              Image.asset('assets/images/nfc_sannkaku.png'),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              Image.asset('assets/images/nfc_sannkaku.png'),
+                              SizedBox(
+                                width: 15,
+                              ),
+                              Stack(
+                                alignment: AlignmentDirectional.center,
+                                children: [
+                                  Container(
+                                    height: 86,
+                                    width: 86,
+                                    decoration: const BoxDecoration(
+                                      shape: BoxShape.circle,
+                                      color: kInputColor,
+                                      //color: kSubBackgroundColor,
+                                    ),
                                   ),
-                                ),
-                                const Text("閉じる",
-                                    style: TextStyle(
-                                        fontSize: 18, color: kFontColor)),
-                              ],
+                                  Image.asset(
+                                      'assets/images/hiyoko_nfc_right.png')
+                                ],
+                              ),
+                            ],
+                          ),
+                widget.success || failed //////////閉じるボタン//////////
+                    ? GestureDetector(
+                        onTap: () {
+                          Navigator.of(context).pop();
+                          Navigator.of(context).pop();
+                          Navigator.of(context).pop();
+                        },
+                        child: Stack(
+                          alignment: Alignment.center,
+                          children: [
+                            Container(
+                              height: 50,
+                              width: 120,
+                              decoration: BoxDecoration(
+                                color: kCanselColor,
+                                borderRadius: BorderRadius.circular(10),
+                              ),
                             ),
-                          )
-                        : GestureDetector(
-                            //////////!success&&!failedならスキャンボタン表示//////////
-                            onTap: () {
-                              _hasClosedWriteDialog = true;
-                              _stream?.cancel();
-                              Navigator.of(context).pop();
-                              Navigator.of(context).pop();
-                            },
-                            child: Stack(
-                              alignment: Alignment.center,
-                              children: [
-                                Container(
-                                  height: 50,
-                                  width: 120,
-                                  decoration: BoxDecoration(
-                                    color: kCanselColor,
-                                    borderRadius: BorderRadius.circular(10),
-                                  ),
-                                ),
-                                const Text("キャンセル",
-                                    style: TextStyle(
-                                        fontSize: 18,
-                                        color: kFontColor,
-                                        fontFamily: 'KiwiMaru-R')),
-                              ],
+                            const Text("閉じる",
+                                style:
+                                    TextStyle(fontSize: 18, color: kFontColor)),
+                          ],
+                        ),
+                      )
+                    : GestureDetector(
+                        //////////!success&&!failedならスキャンボタン表示//////////
+                        onTap: () {
+                          _hasClosedWriteDialog = true;
+                          _stream?.cancel();
+                          Navigator.of(context).pop();
+                          Navigator.of(context).pop();
+                        },
+                        child: Stack(
+                          alignment: Alignment.center,
+                          children: [
+                            Container(
+                              height: 50,
+                              width: 120,
+                              decoration: BoxDecoration(
+                                color: kCanselColor,
+                                borderRadius: BorderRadius.circular(10),
+                              ),
                             ),
-                          )
-                  ],
-                )
+                            const Text("キャンセル",
+                                style: TextStyle(
+                                    fontSize: 18,
+                                    color: kFontColor,
+                                    fontFamily: 'KiwiMaru-R')),
+                          ],
+                        ),
+                      )
               ],
-            ),
-          ),
-        ],
+            )
+          ],
+        ),
       ),
     );
   }

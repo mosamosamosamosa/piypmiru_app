102ee45d4b9e5e713574070bf3c2279c7558cd52
櫻井 雛羽 <2200496@ecc.ac.jp>
2023-02-08 19:55:55 +0900

    ✨ユーザ情報関連のprovider

--- lib/provider/provider.dart
+++ lib/provider/provider.dart
@@ -10,6 +10,11 @@ final StateProvider<int> userProvider = StateProvider((ref) {
   return 0;
 });
 
+//更新可能な値：名前
+final StateProvider<String> nameProvider = StateProvider((ref) {
+  return "";
+});
+
 //更新可能な値:familyID
 final StateProvider<int> familyProvider = StateProvider((ref) {
   return 0;
@@ -18,4 +23,19 @@ final StateProvider<int> familyProvider = StateProvider((ref) {
 //更新可能な値:operationId
 final StateProvider<int> opeProvider = StateProvider((ref) {
   return 0;
+});
+
+//更新可能な値:運転手か。保護者か
+final StateProvider<bool> roleProvider = StateProvider((ref) {
+  return true;
+});
+
+//更新可能な値:mail
+final StateProvider<String> mailProvider = StateProvider((ref) {
+  return '';
+});
+
+//更新可能な値:group
+final StateProvider<String> groupProvider = StateProvider((ref) {
+  return '';
 });
